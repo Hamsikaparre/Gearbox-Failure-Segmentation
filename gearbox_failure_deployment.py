71153b71d4cf39288322d2d073e1c666664466ad
Hamsika Parre <hamsika.parre@gmail.com>
2026-08-21 15:12:18 +0400

    Update gearbox_failure_deployment.py

--- gearbox_failure_deployment.py
+++ gearbox_failure_deployment.py
@@ -31,9 +31,6 @@ input_data = pd.DataFrame({
 if st.button("Predict Gearbox Failure Segmentation"):
     prediction = model.predict(input_data)
 
-    st.write(input_data)
-    st.write("Prediction:", prediction[0])
-
     if prediction[0] == 1:
         st.error("Gear Failure Predicted")
     else:
